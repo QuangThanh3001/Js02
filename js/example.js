90dc378ff6cb98e19150093bab453d515bd3d2a0
QuangThanh3001 <quangthanh33200@gmail.com>
2022-07-26 21:13:20 +0700

    chuot1

---
+++ js/example.js
@@ -0,0 +1,31 @@
+var greeting = 'Howdy';
+var name = 'Molly';
+var message = ', please check your order:';
+
+var welcome = greeting + name + message;
+
+var sign = 'Montague House';
+var tiles = sign.length;
+var subTotal = titles * 5;
+var  grandTotal = subTotal + shipping;
+
+var el = document.getElementById('greeting');
+el.textContent = welcome;
+
+var  elSign = document.getElementById('userSign');
+elSing.textContent= sign;
+
+var elTitles = document.getElementById('userSign');
+elSing.textContent = sign;
+
+var elTitles = document.getElementById('tiles');
+elTiles.textContent = tiles;
+
+var elSubTotal = document.getElementById('subTotal');
+elSubTotal.textContent = '$' + subTotal;
+
+var elShipping = document.getElementById('shipping');
+elShipping.textContent= '$' + shipping;
+
+var elGrandTotal = document.getElementById('grandTotal');
+elGrandTotal.textContent='$' + grandTotal;
---
+++ js/string-variable.js
@@ -0,0 +1,11 @@
+var usename;
+var message;
+
+usename='Molly';
+message='See our upcoming range';
+
+var elName = document.getElementById('name');
+elName.textContent= usename;
+
+var elNote = document.getElementById('note');
+elNote.textContent = message;
